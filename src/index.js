dfa34e5673a3bcd760cfebf4b25486f0880edf5d
Jossani da Cruz <jossanidacruz2@gmail.com>
2022-07-15 17:45:36 +0100

    finished the project, maybe in the future i'll implement some updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-// #3e3e40
-// #d94a56
-// #f2f2f2
+import quotes from './quotes.js';
+
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const newQuote = document.getElementById('new-quote');
+
+
+function generateQuote() {
+
+  // return a random index in the range of 0 and quote lenght
+
+  const quoteIndex = Math.floor(Math.random() * (quotes.length - 1));
+  const generatedQuote = quotes[quoteIndex];
+
+  quote.textContent = `"${generatedQuote.quote}"`;
+  author.textContent = generatedQuote.author;
+
+  console.log(quotes.length);
+
+}
+
+
+newQuote.onclick = generateQuote;
+window.onload = generateQuote;
