a270bd2f28028c5365fef7e8a758c5f3a79153cc
Jossani da Cruz <jossanidacruz2@gmail.com>
2022-07-15 17:02:40 +0100

    added the array of objects full of quotes that will be displayed dinamically

---
+++ src/quotes.js
@@ -0,0 +1,392 @@
+const quotes = [
+
+  {
+
+    quote: 'Levante a cabeça e prossiga com a vida. Você tem suas próprias pernas para andar. Use-as!',
+    author: 'Fullmetal Alchemist'
+
+  },
+
+  {
+
+    quote: 'Exige muito de ti e espera pouco dos outros. Assim, evitarás muitos aborrecimentos',
+    author: 'Confúcio'
+
+  },
+
+  {
+
+    quote: 'Daria tudo que sei pela metade do que ignoro',
+    author: 'René Descartes'
+
+  },
+
+  {
+
+    quote: 'Eu não sei o que quero ser, mas sei muito bem o que não quero me tornar',
+    author: 'Friedrich Nietzsche'
+
+  },
+
+  {
+
+    quote: 'Não fales bem de ti aos outros, pois não os convencerás. Não fales mal, pois te julgarão muito pior do que és',
+    author: 'Confúcio'
+
+  },
+  
+  {
+    
+    quote: 'Nada é suficiente para quem considera pouco o suficiente',
+    author: 'Epicuro'
+
+  },
+
+  {
+    
+    quote: 'Para conhecermos os amigos é necessário passar pelo sucesso e pela desgraça. No sucesso, verificamos a quantidade e, na desgraça, a qualidade',
+    author: 'Confúcio'
+
+  },
+
+  {
+    
+    quote: 'Não espere por uma crise para descobrir o que é importante em sua vida',
+    author: 'Platão'
+
+  },
+
+  {
+    
+    quote: 'Sábio é aquele que conhece os limites da própria ignorância',
+    author: 'Sócrates'
+
+  },
+
+  {
+    
+    quote: 'Até que o sol não brilhe, acendamos uma vela na escuridão',
+    author: 'Confúcio'
+
+  },
+
+  {
+    
+    quote: 'Só sei que nada sei, e o fato de saber isso, me coloca em vantagem sobre aqueles que acham que sabem alguma coisa',
+    author: 'Sócrates'
+
+  },
+
+  {
+    
+    quote: 'Dedica-se a esperar o futuro apenas quem não sabe viver o presente',
+    author: 'Sêneca'
+
+  },
+
+  {
+    
+    quote: 'Não se pode acreditar que é possível ser feliz procurando a infelicidade alheia',
+    author: 'Sêneca'
+
+  },
+
+  {
+    
+    quote: 'Espera de teu filho o que fizeste com teu pai',
+    author: 'Tales de Mileto'
+
+  },
+
+  {
+    
+    quote: 'Um homem esperto cria mais oportunidades do que encontra',
+    author: 'Francis Bacon'
+
+  },
+
+  {
+    
+    quote: 'Seja mais',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Apesar do medo, termine o trabalho',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Se você não acredita em si mesmo, ninguém fará isso por você',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Se você me vir numa briga com um urso, reze para o urso',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Grandes coisas vêm do trabalho duro e perseverança. Sem desculpas',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Descanse no final, não no meio',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Não consigo me identificar com pessoas preguiçosas. Nós não falamos o mesmo idioma',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Eu sangrei roxo e dourado a vida toda. Isso é tudo que eu sei',
+    author: 'Kobe Bryant'
+
+  },
+
+  {
+    
+    quote: 'Seja homem possas',
+    author: 'Cachorrão'
+
+  },
+
+  {
+    
+    quote: 'Trabalhe arduamente e nunca deixe de sonhar',
+    author: 'Goku'
+
+  },
+
+  {
+    
+    quote: 'Eu prefiro ser um macaco sem cérebro, do que um monstro sem coração',
+    author: 'Goku'
+
+  },
+
+  {
+    
+    quote: 'O poder surge em resposta a uma necessidade, não em resposta a um desejo',
+    author: 'Goku'
+
+  },
+
+  {
+    
+    quote: 'Que desgraça é para o homem envelhecer sem nunca ver a beleza e a força do que o seu corpo é capaz',
+    author: 'Sócrates'
+
+  },
+
+  {
+    
+    quote: 'Pouco conhecimento faz com que as pessoas se sintam orgulhosas. Muito conhecimento, que se sintam humildes. É assim que as espigas sem grãos erguem desdenhosamente a cabeça para o céu, enquanto que as cheias as baixam para a terra, sua mãe',
+    author: 'Leonardo da Vinci'
+
+  },
+
+  {
+    
+    quote: 'Se eu não fizer, ninguém vai fazer por mim',
+    author: 'Anon'
+
+  },
+
+  {
+    
+    quote: 'Você é um ser incrível, eu te admiro muito porque deu o melhor de sí',
+    author: 'Goku'
+
+  },
+
+  {
+    
+    quote: 'A suprema arte da guerra é derrotar o inimigo sem lutar',
+    author: 'Sun Tzu'
+
+  },
+
+  {
+    
+    quote: 'A estratégia sem tática é o caminho mais lento para a vitória. Tática sem estratégia é o ruído antes da derrota',
+    author: 'Sun Tzu'
+
+  },
+
+  {
+    
+    quote: 'A vitória está reservada para aqueles que estão dispostos a pagar o preço',
+    author: 'Sun Tzu'
+
+  },
+
+  {
+    
+    quote: 'A habilidade de alcançar a vitória mudando e adaptando-se de acordo com o inimigo é chamada de genialidade',
+    author: 'Sun Tzu'
+
+  },
+
+  {
+    
+    quote: 'Conhece ao teu inimigo e a ti mesmo e nem em cem batalhas conhecerás a derrota',
+    author: 'Sun Tzu'
+
+  },
+
+  {
+    
+    quote: 'Está morto: podemos elogiá-lo à vontade',
+    author: 'Machado de Assis'
+
+  },
+
+  {
+    
+    quote: 'Aja antes de falar e, portanto, fale de acordo com os seus atos',
+    author: 'Confúcio'
+
+  },
+
+  {
+    
+    quote: 'Não corrigir nossas faltas é o mesmo que cometer novos erros',
+    author: 'Confúcio'
+
+  },
+
+
+  {
+    
+    quote: 'Quando vires um homem bom, tenta imitá-lo; quando vires um homem mau, examina-te a ti mesmo',
+    author: 'Confúcio'
+
+  },
+
+  {
+    
+    quote: 'Transportai um punhado de terra todos os dias e fareis uma montanha',
+    author: 'Confúcio'
+
+  },
+
+  {
+    
+    quote: 'Na vida para ter sucesso é preciso sangrar',
+    author: 'Félix Quituto'
+
+  },
+
+  {
+    
+    quote: 'Quanto mais escura a noite mais brilhante as estrelas',
+    author: 'Braum'
+
+  },
+
+  {
+    
+    quote: 'A escalada é longa, mas a vista vale a pena!',
+    author: 'Taric'
+
+  },
+
+  {
+    
+    quote: 'Eu prefiro errar do que não fazer nada',
+    author: 'Ekko'
+
+  },
+
+  {
+    
+    quote: 'Posso falhar, mas não vou desistir',
+    author: 'Ryze'
+
+  },
+
+  {
+    
+    quote: 'Se estiverem esperando que eu desista, é bom esperarem sentados',
+    author: 'Poppy'
+
+  },
+
+  {
+    
+    quote: '⁠As pessoas buscam por uma força que já existe dentro delas',
+    author: 'Pantheon'
+
+  },
+
+  {
+    
+    quote: 'Uma mente concentrada pode perfurar rochas',
+    author: 'Master Yi'
+
+  },
+
+  {
+    
+    quote: '⁠Eu me esforço. Eu falho. Mas a rendição é um privilégio que eu não possuo',
+    author: 'Lee Sin'
+
+  },
+
+  {
+    
+    quote: 'O fracasso não me impediu antes, e não vai me impedir agora',
+    author: 'Lee Sin'
+
+  },
+
+  {
+    
+    quote: 'Se deseja ser nada, continue fazendo nada',
+    author: 'Camille'
+
+  },
+
+  {
+    
+    quote: 'Não importa como você bate, e sim o quanto aguenta apanhar e continuar lutando; o quanto pode suportar e seguir em frente. É disso que a vitória é feita',
+    author: 'Rocky Balboa'
+
+  },
+
+  {
+    
+    quote: "Faça ou não faça. Não existe 'tentar'",
+    author: 'Mestre Yoda'
+
+  },
+
+  {
+    
+    quote: 'Por baixo desta máscara há uma ideia, e ideias são à prova de bala',
+    author: 'V'
+
+  }
+
+];
+
+
+export default quotes;
